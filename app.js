842878394672967ff5bf056feabd2b03c941b9b5
Azis R <alfatihstudi@gmail.com>
2023-12-27 02:33:21 +0700

    Section 5: Working with Express.js

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-const http = require('http');
-const routes = require('./routes');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-const server = http.createServer(routes.handler);
+const app = express();
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
